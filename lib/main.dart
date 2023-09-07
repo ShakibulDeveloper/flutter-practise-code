298608c01bd194727f58c43eff370e01e732d12f
ShakibulDeveloper <epiktheme@gmail.com>
2023-09-07 18:27:13 +0600

    Class Two

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/cupertino.dart'; // for ios
-import 'package:flutter/material.dart'; // for andorid
+import 'package:flutter/material.dart';
 
 void main(){
-  // Hot Reload => ctrl+s
-  // Hot Restart => ctrl + alt + \
   runApp(const MyApp());
 }
 
@@ -14,47 +11,65 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeScreen(),
-      title: 'Flutter App',
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Mobile App',
     );
   }
   
 }
 
 class HomeScreen extends StatelessWidget{
-  const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurpleAccent,
       appBar: AppBar(
-        title: Text("Home"),
-        centerTitle: true,
-        /*
-          custom color use => Color(colorcode),
-          readymate color use => Colors.colorname
-         */
-        leading: Icon(Icons.person, color: Colors.yellow, size: 28,),
+        title: Text('Home'),
       ),
-
-      body:Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Hello World!", style: TextStyle(
-            fontSize: 32,
-            color: Colors.red,
-            fontWeight: FontWeight.w900,
-          ),),
-          Text("Hello World!", style: TextStyle(
-            fontSize: 25,
-            color: Colors.lightGreen,
-            fontWeight: FontWeight.w500,
-          ),),
-          Text("Hello World!", style: TextStyle(
-            fontSize: 32,
-            color: Colors.red,
-            fontWeight: FontWeight.w900,
-          ),),
+          Text("Hello World!"),
+          Text("Hello World!"),
+          Text("Hello World!"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Good ', style: TextStyle(color: Colors.red),),
+            Text('Morning ', style: TextStyle(color: Colors.red)),
+            Text('Flutter ', style: TextStyle(color: Colors.red)),
+          ],
+        ),
+          Column(
+            children: [
+              Image.network('https://images.unsplash.com/photo-1682687220063-4742bd7fd538?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1375&q=80',
+              width: 250,
+              fit: BoxFit.cover,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('images/Koala.jpg', width: 82, fit: BoxFit.cover,), // local img
+                  Image.asset('images/Penguins.jpg', width: 82, fit: BoxFit.scaleDown,), // local img
+                  Image.asset('images/Koala.jpg', width: 82, fit: BoxFit.scaleDown,) // local img
+                ],
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              RichText(text: TextSpan(
+                children: [
+                  TextSpan(text: 'Already a member? Click Here: '),
+                  TextSpan(text: 'Sign in', style: TextStyle(color: Colors.blue)),
+                ],
+              )),
+            ],
+          ),
         ],
       ),
     );
