81dcd2d1ea3a859734bae7cb088f79cbb5c8dc6f
ShakibulDeveloper <epiktheme@gmail.com>
2023-09-21 23:24:21 +0600

    Cupertino Basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,68 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CupertinoApp(
       home: HomeScreen(),
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.indigoAccent,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green,
-            padding: EdgeInsets.all(20),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50),
-            ),
-            textStyle: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.green,
-            textStyle: TextStyle(
-              fontSize: 16,
-            ),
-          ),
-        ),
-        textTheme: TextTheme(
-          bodyLarge: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.w900,
-          ),
-          bodyMedium: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w400,
-          ),
-          bodySmall: TextStyle(
-            fontSize: 14,
-            color: Colors.black38,
-          ),
-        ),
-      ),
-      darkTheme: ThemeData(
-        textTheme: TextTheme(
-          bodyLarge: TextStyle(
-            fontSize: 28,
-            color: Colors.red,
-          ),
-        ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.red,
-        ),
-      ),
-      // themeMode: ThemeMode.dark,
-      themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
     );
   }
 
 }
 
 class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Flutter App", style: Theme.of(context).textTheme.bodyLarge,),
-            Text("version 3", style: Theme.of(context).textTheme.bodyMedium,),
-            Text("Made by Shakibul", style: Theme.of(context).textTheme.bodySmall,),
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          leading: Icon(CupertinoIcons.back),
+          middle: Text("Home"),
+          trailing: CupertinoSwitch(
+              value: false,
+              activeColor: CupertinoColors.link,
+              onChanged: (bool value){
 
-            SizedBox(height: 10,),
-            TextButton(onPressed: (){}, child: Text("click here"),),
-            TextButton(onPressed: (){}, child: Text("click here"),),
-            TextButton(onPressed: (){}, child: Text("click here"),),
-            ElevatedButton(onPressed: (){}, child: Text("Approve"),),
-
-            SizedBox(height: 5),
-            ElevatedButton(onPressed: (){}, child: Text("Remove"), style:
-               ElevatedButton.styleFrom(
-                 backgroundColor: Colors.red,
-               )
-              ),
-          ],
+              },
+          ),
         ),
-      ),
+        child: Center(
+          child: ListView(
+            children: [
+              SizedBox(height: 100,),
+              Column(
+                children: [
+                  Text("Hello Apple"),
+                  Text("Hello Flutter"),
+                  CupertinoButton(child: Text("Click Me"), onPressed: (){}),
+                  CupertinoButton.filled(child: Text("Click Me"), onPressed: (){})
+                ],
+              ),
+              SizedBox(height: 100,),
+              Column(
+                children: [
+                  CupertinoTabBar(
+                    items: [
+                      BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: "Home",),
+                      BottomNavigationBarItem(icon: Icon(CupertinoIcons.mail), label: "Mail",),
+                      BottomNavigationBarItem(icon: Icon(CupertinoIcons.phone), label: "Phone",),
+                      BottomNavigationBarItem(icon: Icon(CupertinoIcons.map), label: "Map",),
+                    ],
+                  ),
+                ],
+              ),
+
+            ],
+          ),
+        ),
     );
   }
 
